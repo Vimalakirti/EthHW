3009771cd21dc56c8fcd08f44a5f06fe04275cfd
Vimalakirti <jade@LAPTOP-TN0HDTFH.localdomain>
2019-10-29 23:11:28 +0800

    README

--- 3_TodoApp/react/src/index.js
+++ 3_TodoApp/react/src/index.js
@@ -1,14 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import getWeb3 from "./utils/getWeb3";
-
-import TodoAppContract from "../../build/contracts/TodoApp.json"
-
-import TodoHeader from './pages/TodoHeader';
-import TodoForm from './pages/TodoForm';
-import TodoList from './pages/TodoList';
-import './index.css';
-
 /* Reference: https://codepen.io/marekdano/pen/bVNYpq
 Todo app structure
 
@@ -21,6 +10,17 @@ TodoApp
 		- TodoListItem #N
 	- TodoForm
 */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import getWeb3 from "./utils/getWeb3";
+
+import TodoAppContract from "../../build/contracts/TodoApp.json"
+
+import TodoHeader from './pages/TodoHeader';
+import TodoForm from './pages/TodoForm';
+import TodoList from './pages/TodoList';
+import './index.css';
 
 var todoItems = [];
   
@@ -35,25 +35,16 @@ class TodoApp extends React.Component {
 
   componentDidMount = async () => {
     try {
-      // Get network provider and web3 instance.
       const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
       const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
       const networkId = await web3.eth.net.getId();
       const deployedNetwork = TodoAppContract.networks[networkId];
       const instance = new web3.eth.Contract(
         TodoAppContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
       this.setState({ web3, accounts, contract: instance });
     } catch (error) {
-      // Catch any errors for any of the above operations.
       alert(
         `Failed to load web3, accounts, or contract. Check console for details.`,
       );
@@ -61,20 +52,12 @@ class TodoApp extends React.Component {
     }
   };
 
-  /* YOUR CODE HERE*/
-  /* An EXAMPLE FOR YOU */
-  contractAddTodo = async (v) => {
-    const { accounts, contract } = this.state;
-    await contract.methods.addTodo(v).send({ from: accounts[0] });
-  };
+  /* YOUR CODE HERE */
+  // Maybe you sould take a look at https://github.com/truffle-box/react-box
 
   
-  
-
-  /* END */
 
   addItem (todoItem) {
-    //this.contractAddTodo(todoItem.newItemValue);
     todoItems.unshift({
       index: todoItems.length+1, 
       value: todoItem.newItemValue, 
@@ -89,11 +72,22 @@ class TodoApp extends React.Component {
   markTodoDone(itemIndex) {
     var todo = todoItems[itemIndex];
     todoItems.splice(itemIndex, 1);
+    if (todo.done) {
+
+    } else {
+
+    }
     todo.done = !todo.done;
     todo.done ? todoItems.push(todo) : todoItems.unshift(todo);
     this.setState({todoItems: todoItems});  
   }
+
+  /* END OF YOUR CODE */
+
   render() {
+    if (!this.state.web3) {
+      return <div>Loading Web3, accounts, and contract...</div>;
+    }
     return (
       <div id="main">
         <TodoHeader />
